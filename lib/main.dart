5b21ee42200c388b50872d25ad471f1cb276a71b
Zakir Hussain <48909627+zhzakir@users.noreply.github.com>
2020-08-25 11:51:39 +0600

    Updated main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:bill_calculator/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(
-      home: BillSplitter(),
-    ));
+void main() => runApp(BillSplitter());
