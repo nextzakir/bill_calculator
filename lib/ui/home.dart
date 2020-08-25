87e7797644204d6f38e8fbeba7bb93a52beb12ab
Zakir Hussain <48909627+zhzakir@users.noreply.github.com>
2020-08-25 11:53:57 +0600

    Updated home.dart file.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -13,234 +13,236 @@ class _BillSplitterState extends State<BillSplitter> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.greenAccent.shade100,
-        title: Text(
-          'Tip Calculator',
-          style: TextStyle(
-            color: Colors.green.shade900,
-            fontWeight: FontWeight.bold,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent.shade100,
+          title: Text(
+            'Tip Calculator',
+            style: TextStyle(
+              color: Colors.green.shade900,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
-        color: Colors.white,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          padding: EdgeInsets.all(20),
-          children: <Widget>[
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.greenAccent.shade100,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Total Per Person',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.green.shade900,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        '\$${calculateTotalPerPerson(_billAmount, _personCounter, _tipPercentage)}',
+        body: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(top: 10.0),
+          color: Colors.white,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            padding: EdgeInsets.all(20),
+            children: <Widget>[
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.greenAccent.shade100,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Total Per Person',
                         style: TextStyle(
-                          fontSize: 30,
+                          fontSize: 20,
                           color: Colors.green.shade900,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          '\$${calculateTotalPerPerson(_billAmount, _personCounter, _tipPercentage)}',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.green.shade900,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(
+                    color: Colors.blueGrey.shade100,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextField(
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        style: TextStyle(
+                          color: Colors.green.shade900,
+                        ),
+                        decoration: InputDecoration(
+                          prefixText: "Bill Amount: ",
+                        ),
+                        onChanged: (String value) {
+                          try {
+                            _billAmount = double.parse(value);
+                          } catch (exception) {
+                            _billAmount = 0.0;
+                          }
+                        },
+                      ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Split",
+                            style: TextStyle(
+                              color: Colors.green.shade900,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (_personCounter > 1) {
+                                      _personCounter--;
+                                    } else {
+                                      // Do Nothing!
+                                    }
+                                  });
+                                },
+                                child: Container(
+                                  width: 30,
+                                  height: 30,
+                                  margin: EdgeInsets.only(right: 10.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(7.0),
+                                    color: Colors.greenAccent.shade100,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "-",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.green.shade900,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "$_personCounter",
+                                style: TextStyle(
+                                  color: Colors.green.shade900,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (_personCounter >= 1) {
+                                      _personCounter++;
+                                    } else {
+                                      // Do Nothing!
+                                    }
+                                  });
+                                },
+                                child: Container(
+                                  width: 30,
+                                  height: 30,
+                                  margin: EdgeInsets.only(left: 10.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(7.0),
+                                    color: Colors.greenAccent.shade100,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "+",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.green.shade900,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Tip",
+                            style: TextStyle(
+                              color: Colors.green.shade900,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "\$${(calculateTotalTip(_billAmount, _tipPercentage, _personCounter).toStringAsFixed(2))}",
+                            style: TextStyle(
+                              color: Colors.green.shade900,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Column(
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              "$_tipPercentage%",
+                              style: TextStyle(
+                                  color: Colors.green.shade900,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Slider(
+                            min: 0,
+                            max: 100,
+                            divisions: 10,
+                            activeColor: Colors.green.shade900,
+                            inactiveColor: Colors.grey,
+                            value: _tipPercentage.toDouble(),
+                            onChanged: (double newValue) {
+                              setState(() {
+                                _tipPercentage = newValue.round();
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 20.0),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(
-                  color: Colors.blueGrey.shade100,
-                  style: BorderStyle.solid,
-                ),
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: TextField(
-                      keyboardType:
-                          TextInputType.numberWithOptions(decimal: true),
-                      style: TextStyle(
-                        color: Colors.green.shade900,
-                      ),
-                      decoration: InputDecoration(
-                        prefixText: "Bill Amount: ",
-                      ),
-                      onChanged: (String value) {
-                        try {
-                          _billAmount = double.parse(value);
-                        } catch (exception) {
-                          _billAmount = 0.0;
-                        }
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Split",
-                          style: TextStyle(
-                            color: Colors.green.shade900,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Row(
-                          children: <Widget>[
-                            InkWell(
-                              onTap: () {
-                                setState(() {
-                                  if (_personCounter > 1) {
-                                    _personCounter--;
-                                  } else {
-                                    // Do Nothing!
-                                  }
-                                });
-                              },
-                              child: Container(
-                                width: 30,
-                                height: 30,
-                                margin: EdgeInsets.only(right: 10.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(7.0),
-                                  color: Colors.greenAccent.shade100,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    "-",
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.green.shade900,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Text(
-                              "$_personCounter",
-                              style: TextStyle(
-                                color: Colors.green.shade900,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w900,
-                              ),
-                            ),
-                            InkWell(
-                              onTap: () {
-                                setState(() {
-                                  if (_personCounter >= 1) {
-                                    _personCounter++;
-                                  } else {
-                                    // Do Nothing!
-                                  }
-                                });
-                              },
-                              child: Container(
-                                width: 30,
-                                height: 30,
-                                margin: EdgeInsets.only(left: 10.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(7.0),
-                                  color: Colors.greenAccent.shade100,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    "+",
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.green.shade900,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Tip",
-                          style: TextStyle(
-                            color: Colors.green.shade900,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "\$${(calculateTotalTip(_billAmount, _tipPercentage, _personCounter).toStringAsFixed(2))}",
-                          style: TextStyle(
-                            color: Colors.green.shade900,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Column(
-                      children: <Widget>[
-                        Center(
-                          child: Text(
-                            "$_tipPercentage%",
-                            style: TextStyle(
-                                color: Colors.green.shade900,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Slider(
-                          min: 0,
-                          max: 100,
-                          divisions: 10,
-                          activeColor: Colors.green.shade900,
-                          inactiveColor: Colors.grey,
-                          value: _tipPercentage.toDouble(),
-                          onChanged: (double newValue) {
-                            setState(() {
-                              _tipPercentage = newValue.round();
-                            });
-                          },
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
